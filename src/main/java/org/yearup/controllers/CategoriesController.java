c13307d5111ebf9f5d0de9b03c55a6a3b6b8d71d
Student <enesyilmaz4525@gmail.com>
2024-12-16 11:22:00 -0500

    @RestController has been added to CategoriesController class.

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 // add the annotations to make this a REST controller
+@RestController
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
